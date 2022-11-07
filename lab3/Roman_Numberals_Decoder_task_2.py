13bf2be4da7b15f64d899895c055b0dc7b08597c
PrtagonistOne <playerdna4@gmail.com>
2022-11-07 18:58:12 +0200

    Lab3 done all at once

--- lab3/Roman_Numberals_Decoder_task_2.py
+++ lab3/Roman_Numberals_Decoder_task_2.py
@@ -3,11 +3,11 @@ roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 
 def roman_to_int(S: str) -> int:
-    sum = 0
+    _sum = 0
     for i in range(len(S) - 1, -1, -1):
         num = roman[S[i]]
-        sum = sum - num if 3 * num < sum else sum + num
-    return sum
+        _sum = _sum - num if 3 * num < _sum else _sum + num
+    return _sum
 
 
 print(roman_to_int('III'))
