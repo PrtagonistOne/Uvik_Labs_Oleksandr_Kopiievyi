bdc0a5302fac3afb260a01a1b09d1de6143dba87
PrtagonistOne <playerdna4@gmail.com>
2022-11-12 18:30:52 +0200

    Lab 8.2, modified isbn_validator done

--- lab8/isbn_validator_modified.py
+++ lab8/isbn_validator_modified.py
@@ -10,42 +10,22 @@ class ISBNValidationError(Exception):
 
 @dataclass
 class ISBNValidator:
-    isbn: str
+    def __call__(self, isbn):
+        """Change the position of the entity."""
+        print('Validating..')
+        if len(isbn) != 10:
+            raise ISBNValidationError
 
-    @staticmethod
-    def isbn_validator(func):
-        @wraps(func)
-        def wrapper(isbn: str):
-            print('Validating..')
-            if len(isbn) != 10:
-                raise ISBNValidationError
-
-            nums = [10 if num == "X" else int(num) for num in isbn]
-            checked = sum(nums[num - 1] * num for num in range(1, 11)) % 11
-            if checked:
-                raise ISBNValidationError
-            return func(isbn)
-
-        return wrapper
-
-    @staticmethod
-    def connect_to_isbn_address(isbn: str):
-        return f"Successfully connected to {isbn}!"
-
-    def __call__(self):
-        '''Change the position of the entity.'''
-        self.isbn_validator(self.connect_to_isbn_address(self.isbn))
+        nums = [10 if num == "X" else int(num) for num in isbn]
+        checked = sum(nums[num - 1] * num for num in range(1, 11)) % 11
+        if checked:
+            raise ISBNValidationError
         print('Validated Successfully')
+
+        return isbn
 
 
 if __name__ == "__main__":
-    validate = ISBNValidator(isbn='048665088X')
-    validate()
-
-    validate.isbn = '1112223339X'
-
-    validate()
-
-    # print(connect_to_isbn_address('048665088X'), '\n')
-    #
-    # print(connect_to_isbn_address('1112223339X'))
+    validate_isbn = ISBNValidator()
+    validate_isbn('048665088X')
+    validate_isbn('1112223339X')
