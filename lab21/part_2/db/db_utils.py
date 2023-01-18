c4dfba08c44e756ed24a1ebc27affcfb2ab01545
PrtagonistOne <playerdna4@gmail.com>
2023-01-18 18:56:00 +0200

    part 2 minor refactor

--- lab21/part_2/db/db_utils.py
+++ lab21/part_2/db/db_utils.py
@@ -9,47 +9,61 @@ def get_connection(db_file):
     return None
 
 
+def make_db():
+    connection = get_connection("../posts.sqlite")
+
+    if connection is not None:
+        sql_create_post_table = """ CREATE TABLE IF NOT EXISTS post (
+                                               id integer PRIMARY KEY,
+                                               title text,
+                                               body text NOT NULL,
+                                               likes int
+                                           ); """
+
+        apply_sql(conn=connection, sql_statement=sql_create_post_table)
+
+
 def apply_sql(conn, sql_statement: str, row_values=None):
     c = conn.cursor()
 
-    try:
-        if row_values is not None:
-            c.execute(sql_statement, row_values)
-            conn.commit()
-        else:
-            c.execute(sql_statement)
-            return c
-    except Exception as e:
-        print(e)
+    if row_values is not None:
+        c.execute(sql_statement, row_values)
+        conn.commit()
+    else:
+        c.execute(sql_statement)
+        return c
 
     c.close()
     return c.lastrowid
 
 
 def get_read_connection_data(sql_statement: str) -> dict:
-    conn = get_connection('posts.sqlite')
+    conn = get_connection('../posts.sqlite')
 
     conn.row_factory = sqlite3.Row
-    rows = apply_sql(conn=conn, sql_statement=sql_statement).fetchall()
+    try:
+        rows = apply_sql(conn=conn, sql_statement=sql_statement).fetchall()
+    except AttributeError:
+        return {'Error Message': 'There is no records yet.'}
     return [dict(row) for row in rows]
 
 
 def delete_record(sql_statement: str) -> None:
-    conn = get_connection('posts.sqlite')
+    conn = get_connection('../posts.sqlite')
 
     with conn:
         apply_sql(conn=conn, sql_statement=sql_statement)
 
 
 def perform_update(sql_statement: str, row_data: tuple) -> None:
-    conn = get_connection('posts.sqlite')
+    conn = get_connection('../posts.sqlite')
 
     with conn:
         apply_sql(conn=conn, sql_statement=sql_statement, row_values=row_data)
 
 
 def perform_create(sql_statement: str, post_data: dict) -> int:
-    conn = get_connection('posts.sqlite')
+    conn = get_connection('../posts.sqlite')
     title = post_data['title']
     body = post_data['body']
     likes = post_data['likes']
@@ -60,20 +74,14 @@ def perform_create(sql_statement: str, post_data: dict) -> int:
 
 
 if __name__ == "__main__":
-    connection = get_connection("posts.sqlite")
-
-    sql_create_post_table = """ CREATE TABLE IF NOT EXISTS post (
-                                            id integer PRIMARY KEY,
-                                            title text,
-                                            body text NOT NULL,
-                                            likes int
-                                        ); """
-
+    connect = get_connection("../posts.sqlite")
+    make_db()
     insert_post_sql = ''' INSERT INTO post(title,body,likes)
                   VALUES(?,?,?) '''
 
-    if connection is not None:
-        apply_sql(conn=connection, sql_statement=sql_create_post_table)
-        apply_sql(conn=connection, sql_statement=insert_post_sql, row_values=('some title', 'some body', '10'))
+    if connect is not None:
+
+        apply_sql(conn=connect, sql_statement=insert_post_sql, row_values=('some title', 'some body', '10'))
     else:
-        print('No connection was made!')
+        print('No connection was not made!')
+
